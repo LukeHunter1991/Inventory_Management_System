d1947b960de5f72aecc272153325e70bf5cd82a6
Luke Hunter <luke.hunter9@gmail.com>
2024-05-27 21:01:05 +1000

    Added schema and seed data

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,38 @@
 DROP DATABASE IF EXISTS inventory_db;
 CREATE DATABASE inventory_db;
+
+\c inventory_db;
+
+-- Creating the categories table
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+-- Creating the employees table
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
+-- Creating the items table
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    item_name VARCHAR(100) NOT NULL,
+    description VARCHAR(255),
+    available BOOLEAN DEFAULT TRUE,
+    category_id INTEGER REFERENCES categories(id)
+);
+
+-- Creating the transactions table
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    borrow_date TIMESTAMP NOT NULL,
+    return_date TIMESTAMP,
+    user_id INTEGER REFERENCES employees(id),
+    items_id INTEGER REFERENCES items(id)
+);
