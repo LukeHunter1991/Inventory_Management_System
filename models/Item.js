90eeaa1e26cabd5a14195d08221140cc68015989
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-05-27 21:36:41 +1000

    Added Item Model and Index.js

---
+++ models/Item.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Item extends Model {}
+
+Item.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    item_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    item_description: {
+      type: DataTypes.TEXT,
+    },
+    is_available: {
+      type: DataTypes.BOOLEAN,
+    },
+    category_id: {
+      //Foreign Key referencing the category table id
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'item',
+  }
+);
+
+module.exports = Item;
