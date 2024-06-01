b9cf94970e658f7894fb35eb8d92cee80171e786
Luke Hunter <luke.hunter9@gmail.com>
2024-06-01 19:00:59 +1000

    Updated All Items table in admin route

--- controllers/api/adminRoutes.js
+++ controllers/api/adminRoutes.js
@@ -65,6 +65,7 @@ router.get('/viewsummary', async (req, res) => {
       empNames: empNames,
       txncount: txncount,
       logged_in: req.session.logged_in,
+      is_admin: req.session.is_admin,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -80,8 +81,8 @@ router.get('/allitems', async (req, res) => {
   console.log('ADMIN ITEMS REQUEST');
   try {
     const items = await Item.findAll({
-      where: {
-        is_available: true,
+      include: {model: Category,
+      attributes: ['name'],
       },
     });
     const unborrowedItemData = items.map((item) => item.get({ plain: true }));
@@ -123,20 +124,24 @@ Route to add a new item to the table
 router.post('/add-item', async (req, res) => {
   // create a new category
   try {
-    const categoryId = parseInt(req.body.category_id);
-    const is_available = req.body.is_available == 'true';
+    // Gets id for category_id column
+    const categoryId = await Category.findOne({
+      where: {
+        name: req.body.name,
+      },
+      raw: true,
+    });
 
     // Create object with all required data to create a new row in item table.
     const itemData = {
       item_name: req.body.item_name,
       item_description: req.body.item_description,
-      is_available: is_available,
-      category_id: categoryId,
+      is_available: req.body.is_available,
+      category_id: categoryId.id,
     };
 
     // Use create method provided by Sequilize to INSERT new item into Item table.
     const newItem = await Item.create(itemData);
-
     // Returns tag that was created.
     res.status(200).json(newItem);
   } catch (err) {
@@ -161,29 +166,6 @@ router.get('/category', async (req, res) => {
     );
 
     res.render('category-dashboard', {
-      categoryData: categoryData,
-      logged_in: req.session.logged_in,
-      is_admin: req.session.is_admin,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-/* 
-The `/admin/item` endpoint
-Route to display the Create item page.
-*/
-router.get('/item', async (req, res) => {
-  try {
-    const categories = await Category.findAll({});
-
-    // Serialize transaction data so templates can read it
-    const categoryData = categories.map((category) =>
-      category.get({ plain: true })
-    );
-
-    res.render('add-item', {
       categoryData: categoryData,
       logged_in: req.session.logged_in,
       is_admin: req.session.is_admin,
