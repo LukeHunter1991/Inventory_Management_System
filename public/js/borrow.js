fe2b2879c308e9b7724d7a631475711a1f1ff1a7
Sara Zhou <sara@Saras-MacBook-Air.local>
2024-06-01 18:13:15 +1000

    add employee pages

--- public/js/borrow.js
+++ public/js/borrow.js
@@ -1,15 +1,24 @@
-// Placeholder code to connect with front end
-const borrowItem = async () => {
+const createTransactionHandler = async (event) => {
+  event.preventDefault();
+  const item_id = event.target.getAttribute('data-id');
+  const borrow_date = new Date();
+
+  if (item_id) {
     const response = await fetch('/api/employee/borrow', {
-      method: 'POST',
-        // Add body one front end ready
-      //body: JSON.stringify({ itemId }),
-      headers: { 'Content-Type': 'application/json' },
+      method:
+        'POST',
+      body: JSON.stringify({ borrow_date, item_id }),
+      headers: {
+        'Content-Type':
+          'application/json'
+      },
     });
-  
     if (response.ok) {
-      alert('Succes')
+      document.location.replace('/api/employee'); console.log(response);
     } else {
-      alert('Error borrowing item');
+      alert('Failed to create transaction.');
     }
-  };
+  }
+};
+
+document.querySelector('.create-transaction-btn').addEventListener('click', createTransactionHandler);
